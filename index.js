58854beaccf6a95ffde8f4a14075c24224ea5359
Simon Lally <simonlallydev@gmail.com>
2019-08-26 14:42:46 -0400

    get all champions, current free rotation and legacy runes

--- index.js
+++ index.js
@@ -10,5 +10,47 @@ function getSummonerByName(name) {
     })
     .catch(err => {
       console.log(err);
+      return err;
+    });
+}
+
+function getChampRotation() {
+  Axios.get(
+    `https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${API_KEY}`
+  )
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+      return err;
+    });
+}
+
+function getAllChampions() {
+  // TODO cache locally
+
+  const allChampionsUrl =
+    "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
+  Axios.get(allChampionsUrl)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function getAllRunes() {
+  // these runes have not existed in the game for years
+
+  const allRunesUrl =
+    "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/rune.json";
+  Axios.get(allRunesUrl)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
     });
 }
